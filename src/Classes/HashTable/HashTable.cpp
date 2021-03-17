aafcd0797579094b3881932e91dc27d8c8ed013a
Bryan <bryancarolinomb@gmail.com>
2021-03-17 19:43:07 -0300

    Comentários no código da Hash e na Classe CovidInfo

--- src/Classes/HashTable/HashTable.cpp
+++ src/Classes/HashTable/HashTable.cpp
@@ -1,14 +1,14 @@
 #include "HashTable.h"
 
 HashTable::HashTable()
-{
+{//Instancia básica, com 500 posições na tabela
     this->size = 500;
     this->table.resize(size);
     this->collisions = 0;
 }
 
 HashTable::HashTable(unsigned long long _size)
-{
+{//Instancia da Tabela Hash com tamanho pelo usuário
     this->size = _size;
     this->table.resize(size);
     this->collisions = 0;
@@ -20,16 +20,16 @@ void HashTable::insert(CovidInfo *_CI)
 
     for (unsigned long long i = 0; i < size; i++)
     {
-        unsigned long long hashedKey = polynomialRollingHash(*_CI, a);
+        unsigned long long hashedKey = polynomialRollingHash(*_CI, a); //Calcula o valor hasheado associado a chave
 
         if (table[hashedKey].city == "")
-        {
+        {//Se o índice está vazio, insere o valor, e encerra a função
             table[hashedKey] = *_CI;
             this->generatedHashs.push_back(hashedKey);
             break;
         }
         else
-        {
+        {//Caso o contrário, incremenda a e o número de colisões e volta ao início do loop para calcular um novo valor hash
             a++;
             collisions++;
         }
@@ -41,11 +41,11 @@ CovidInfo *HashTable::search(float _cityCode, string _date)
     unsigned long long hashedKey = polynomialRollingHash(_cityCode, _date, 0);
 
     if (table[hashedKey].code == _cityCode and table[hashedKey].date == _date)
-    {
+    { //Se a entrada procurada está no primeiro valor para onde foi hasheada, já é retornada
         return addressof(table[hashedKey]);
     }
     else
-    {
+    { //Se não, ela é buscada pela tabela
         for (CovidInfo i : table)
         {
             if (i.code == _cityCode and i.date == _date)
@@ -59,15 +59,16 @@ CovidInfo *HashTable::search(float _cityCode, string _date)
 
 vector<CovidInfo> HashTable::getCityInfoByKey(unsigned long long key)
 {
-    vector<CovidInfo> c(table);
-    vector<CovidInfo> r;
-    bool foundIt = false;
+    vector<CovidInfo> c(table); //Instancia copia da tabela hash
+    vector<CovidInfo> r; //Instancia futuro retorno com as cidades requeridas
+    bool foundIt = false; //Flag simboliza ter encontrado o bloco de valores
 
-    c.shrink_to_fit();
-    sort(c.begin(), c.end());
+    c.shrink_to_fit(); //Compacta a cópia da tabela ao tamanho do número de valores realmente inseridos, evitando comparações desnecesárias
+    sort(c.begin(), c.end()); //Ordena a cópia da tabela usandoa função sort e um overload do operador < criado para a classe CovidInfo
 
+    //Verifica o nome da cidade para, através da ordenação em ordem alfabética feita anteriormente, diminuir o número de iterações começando a busca do início do vetor ou do final
     if((table[key].city[0] >= 'a' and table[key].city[0] <= 'm') or (table[key].city[0] >= 'A' and table[key].city[0] <= 'M'))
-    {
+    {//Se a cidade desejada começa com as lestras de 'a' a 'm', a iterações ocorrem de 0 para frente
         for(unsigned long long i = 0; i < size; i++)
         {
             if(c[i].city == table[key].city)
@@ -80,7 +81,7 @@ vector<CovidInfo> HashTable::getCityInfoByKey(unsigned long long key)
         }
     }
     else if((table[key].city[0] >= 'n' and table[key].city[0] <= 'z') or (table[key].city[0] >= 'N' and table[key].city[0] <= 'Z'))
-    {
+    {//Se a cidade desejada começa com as lestras de 'n' a 'z', a iterações ocorrem de size-1 para trás
         for(unsigned long long i = size-1; i >= 0; i--)
         {
             if(c[i].city == table[key].city)
@@ -97,7 +98,7 @@ vector<CovidInfo> HashTable::getCityInfoByKey(unsigned long long key)
 }
 
 void HashTable::print(bool writeInFile)
-{
+{//Função de impressão no terminal ou gravação em um arquivo dado
     ofstream arq("saidaTestes.txt");
     cout << endl
          << "A saída sera salva no arquivo : saidaTestes.txt";
@@ -125,10 +126,10 @@ vector<long long int> HashTable::getHashedKeys()
 {
     return this->generatedHashs;
 }
+
 vector<long long int> HashTable::getNRandomElements(int numberOfElements)
 {
     vector<bool> sortedIndexs;
-    //sortedIndexs.reserve(this->size);
     std::random_device device;
     std::mt19937 generator(device());
     //Coloco que os indices a ser gerados vão de 0 ao tamanho da minha hash
@@ -167,14 +168,14 @@ vector<long long int> HashTable::getNRandomElements(int numberOfElements)
 
 unsigned long long HashTable::polynomialRollingHash(CovidInfo _CI, unsigned long long _a)
 {
-    short p = 97;
-    unsigned long long power_of_p = 1;
-    unsigned long long hash_val = 0;
-    string str = _CI.date + to_string(_CI.code);
-    int strSize = str.size();
+    short p = 97; //Primo com número mais próximos do número de caracteres possíveis na string
+    unsigned long long power_of_p = 1; //Inicialização 
+    unsigned long long hash_val = 0; //Inicialização 
+    string str = _CI.date + to_string(_CI.code); //Concatenação das duas keys
+    int strSize = str.size(); //Tamanho total da string concatenada
 
     for (int i = 0; i < strSize; i++)
-    {
+    { //Iterações do algoritmo PRH
         hash_val = ((hash_val + (str[i] - 'a' + 1) * power_of_p) + _a) % size;
         power_of_p = (power_of_p * p) % size;
     }
@@ -184,14 +185,14 @@ unsigned long long HashTable::polynomialRollingHash(CovidInfo _CI, unsigned long
 
 unsigned long long HashTable::polynomialRollingHash(float _cityCode, string _date, unsigned long long _a)
 {
-    short p = 97;
-    unsigned long long power_of_p = 1;
-    unsigned long long hash_val = 0;
-    string str = _date + to_string(_cityCode);
-    int strSize = str.size();
+    short p = 97; //Primo com número mais próximos do número de caracteres possíveis na string
+    unsigned long long power_of_p = 1; //Inicialização
+    unsigned long long hash_val = 0; //Inicialização
+    string str = _date + to_string(_cityCode); //Concatenação das duas keys
+    int strSize = str.size(); //Tamanho total da string concatenada
 
     for (int i = 0; i < strSize; i++)
-    {
+    { //Iterações do algoritmo PRH
         hash_val = ((hash_val + (str[i] - 'a' + 1) * power_of_p) + _a) % size;
         power_of_p = (power_of_p * p) % size;
     }
@@ -213,7 +214,7 @@ long int HashTable::getAllCasesInCity(int cityCode, int &comparisions,float &ela
 
     for(CovidInfo city : this->table){
         if(city.code == cityCode){
-            cases+= city.cases;
+            cases+= city.cases; //Contando a quantidade total de casos encontrados
             comparisions++;
         }
     }
@@ -225,6 +226,6 @@ long int HashTable::getAllCasesInCity(int cityCode, int &comparisions,float &ela
 }
 
 int HashTable::getSize()
-{
+{//Tamanho da tabela
     return this->table.size();
 }
--- src/Classes/HashTable/HashTable.h
+++ src/Classes/HashTable/HashTable.h
@@ -16,34 +16,37 @@ using namespace std;
 class HashTable
 {
     private:
-        unsigned long long size;
-        unsigned long long collisions;
+        unsigned long long size; //Tamanho da tabela hash
+        unsigned long long collisions; //Número de colisões
 
 
-       vector<long long int> generatedHashs;
-        unsigned long long polynomialRollingHash(CovidInfo CI, unsigned long long _a);
-        unsigned long long polynomialRollingHash(float _cityCode, string _date, unsigned long long _a);
+       vector<long long int> generatedHashs; //Valores hash gerados
+        unsigned long long polynomialRollingHash(CovidInfo CI, unsigned long long _a); //Função PRH de hash
+        unsigned long long polynomialRollingHash(float _cityCode, string _date, unsigned long long _a); //Função PRH de hash
 
     public:
 
-        HashTable();
-        HashTable(unsigned long long _size);
+        HashTable(); //Construtor simples
+        HashTable(unsigned long long _size); //Construtor alocando um tamanho dado
 
-        vector<CovidInfo> table;
-        void insert(CovidInfo* _CI);
+        vector<CovidInfo> table; //Tabela hash - vetor de instancias da classe CovidInfo
+        void insert(CovidInfo* _CI); //Função de inserção na tabela
 
-        CovidInfo* search(float _cityCode, string _date);
-        vector<CovidInfo> getCityInfoByKey(unsigned long long key);
+        CovidInfo* search(float _cityCode, string _date); //Função de procura, retorna o valor procurado através de um dado código e data
+        vector<CovidInfo> getCityInfoByKey(unsigned long long key); //Retorna o todas um vetor com todas as informações de uma cidade dado uma chave hasheada
 
-        void print(bool writeInFile);
+        void print(bool writeInFile); //Função de impressão no terminal ou gravação em um arquivo dado
         
-        long int getAllCasesInCity(int cityCode , int &comparisions,float &elapsedTime);
+        long int getAllCasesInCity(int cityCode , int &comparisions,float &elapsedTime); //Retorna a quantidade total de casos por cidade
 
-        vector<long long int> getNHashedKeys(int numberOfKeys);    
+        //Amostragem do valores inseridos; retorna vetor com a quantidade de entradas específicadas no parâmetro
+        vector<long long int> getNHashedKeys(int numberOfKeys); 
         vector<long long int> getHashedKeys();
         vector<long long int> getNRandomElements(int numberOfElements);
-        vector<CovidInfo> getValues();
-        int getSize();
+        //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+        vector<CovidInfo> getValues(); //Retorna o próprio vetor da tabela hash
+        int getSize(); //Retorna o tamanho da tabela, sem cortar lacunas
 };
 #endif
 
